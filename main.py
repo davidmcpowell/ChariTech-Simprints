f24642837d553b20e64c39b16122ab336cd32cc9
davidmpowell <dp483@cam.ac.uk>
2017-03-17 12:54:23 +0000

    Read in image

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ import os
 def classify():
     pass
 
+# this function does stuff
 def train():
     image_labels = get_image_labels()
-
-def get_image_labels():
-    os.listdir(some_path)
 
 def read_png_file(file_name):
     with open(file_name, 'r') as f:
         r = png.Reader(f)
-        print r.read()
+        width, height, values, properties = r.read()
+        image = np.zeros((width, height))
+        row_num = 0
+        for row in values:
+            image[row_num, :] = np.array(row[1])
+            row_num += 1
+        compressed = compress(image)
 
+def compress(values):
+
+    return compressed_vals
 
 if __name__ == '__main__':
     read_png_file('fingerprintClassification/trainingSet/A/f1675_02.png')
