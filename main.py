94dbfa9837023834c4721ba653e5791ce909e20d
davidmpowell <dp483@cam.ac.uk>
2017-03-17 14:55:36 +0000

    It now works (kind of)

--- main.py
+++ main.py
@@ -2,42 +2,53 @@ import numpy as np
 import png
 import os
 from sklearn import tree
+import random
 
 def classify():
-    clf, X, labels = train()
-    predicted_labels = clf.predict(X)
-    results = compare(labels, predicted_labels)
+    classifier, test_data, test_labels = train()
+    predicted_labels = classifier.predict(test_data)
+    results = compare(test_labels, predicted_labels)
     print results
     
 def compare(labels, predicted_labels):
-    correct = sum((1 for i in xrange(len(actual))
+    correct = sum((1 for i in xrange(len(labels))
                      if labels[i] == predicted_labels[i]))
-    return correct / float(len(actual))
+    return correct / float(len(labels))
 
 # this function does stuff
 def train():
     image_labels = get_image_labels()
     classifier = tree.DecisionTreeClassifier()
-    X, image_files = get_training_data()
+    training_data, training_images, test_data, test_images = get_training_and_test_data()
     #extracts file name from relative path
-    file_names = [image_file.split('/')[-1] for image_file in image_files]
+    train_labels = images_to_labels(training_images, image_labels)
+    test_labels = images_to_labels(test_images, image_labels)
+    classifier.fit(training_data, train_labels)
+    return classifier, test_data, test_labels
+
+def images_to_labels(image_list, image_labels):
+    file_names = [image_file.split('/')[-1] for image_file in image_list]
     labels = [image_labels[file_name] for file_name in file_names]
-    clf.fit(X, labels)
-    return clf, X, labels
+    label_converter = {'imageA' : 0, 'imageL' : 1, 'imageR' : 2, 'imageT' : 3, 'imageW' : 4}
+    number_labels = [label_converter[label] for label in labels]
+    return number_labels
 
 def get_all_image_file_names():
     outer_dir =  'fingerprintClassification/trainingSet/'
     labels = ['A', 'L', 'R', 'T', 'W']
     image_files = [os.path.join(outer_dir, label, file_name)
                         for label in labels
-                        for file_name in os.listdir(os.path.join(outer_dir, label))]
+                        for file_name in os.listdir(os.path.join(outer_dir, label))
+                        if random.random() > 0]
     return image_files
 
-def get_training_data():
+def get_training_and_test_data():
     image_files = get_all_image_file_names()
-    data = np.array([read_png_file(image_file) for image_file in image_files])
-    return data, image_files
-
+    training_images = image_files[:len(image_files)/2]
+    test_images = image_files[len(image_files)/2:]
+    training_data = np.array([read_png_file(image_file) for image_file in training_images])
+    test_data = np.array([read_png_file(image_file) for image_file in test_images])
+    return training_data, training_images, test_data, test_images
 
 def get_image_labels():
     outer_dir =  'fingerprintClassification/trainingSet/'
@@ -51,7 +62,6 @@ def get_image_labels():
             dict_fingerprints[file_name] = 'image' + label
 
     return dict_fingerprints
-
 
 def read_png_file(file_name):
     with open(file_name, 'r') as f:
@@ -72,12 +82,11 @@ def compress(values):
         for j in xrange(num_vals):
             small_grid = values[16*i : i*16+16, 16*j: 16*j +16]
             compressed_vals[i, j] = np.mean(small_grid)
-    return compressed
+    compressed_vals = compressed_vals.flatten()
+    return compressed_vals
 
 if __name__ == '__main__':
-    # read_png_file('fingerprintClassification/trainingSet/A/f1675_02.png')
-    print get_training_data()
-   
+    classify()
 
 
 
