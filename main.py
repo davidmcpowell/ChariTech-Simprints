82f288e4a8d40b38644409b90f91fd0ccdb88589
davidmpowell <dp483@cam.ac.uk>
2017-03-17 11:54:36 +0000

    Initial reading

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import numpy as np
+import png
 
 def classify():
     pass
 
-abcd
+
+def read_png_file(file_name):
+    with open(file_name, 'r') as f:
+        r = png.Reader(f)
+        print r.read()
+
+
+if __name__ == '__main__':
+    read_png_file('fingerprintClassification/trainingSet/A/f1675_02.png')
 
